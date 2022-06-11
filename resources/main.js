0a4e37370604c9099bcb57774ce9444ed8c66d7d
s135988js028 <cristina.bairac@yahoo.ro>
2022-06-12 01:28:20 +0300

    Some changes in css and js

--- resources/main.js
+++ resources/main.js
@@ -15,11 +15,21 @@ document.addEventListener("DOMContentLoaded", function() {
             // menuItem.classList.add("selected");
             for(let j = 0; j < menuItems.length; j++) {
                 menuItems[j].classList.remove("selected");
+                menuItems[j].parentNode.classList.remove("active");
+
+
             }
 
             this.classList.add("selected");
+
+            console.log(this);
+
+            this.parentNode.classList.add('active')
             
             const dataContent = this.dataset.content;
+
+
+            const dataLogo = this.dataset.logo;
 
             if (dataContent === 'reward-container' || dataContent === 'account-container' || dataContent === 'payment-container' || dataContent === 'details-container') {
                 document.querySelector('.title-sidebar').classList.remove('hidden');
@@ -30,19 +40,35 @@ document.addEventListener("DOMContentLoaded", function() {
             }
 
             if (dataContent === 'details-container') {
-                // let select = document.getElementsByClassName('menu-item')[0];
-                // select.classList.add("selected");
-
+                document.querySelector('.selected-li').classList.add('active');
             }
+
+            // document.getElementById('details-container').style.display = "none";
+            // document.querySelector('.details-container').style.display = "none";
+            // if (dataContent === 'details-container') {
+            //     document.getElementById('details-container').style.display = "flex";
+            //     document.querySelector('.details-container').style.display = "flex";
+            // }
+
 
             const contentElements = document.getElementsByClassName("content");
             for(let k = 0; k < contentElements.length; k++) {
                 contentElements[k].classList.add("hidden");
             }
 
+            const logoElements = document.getElementsByClassName("logo");
+            for(let k = 0; k < logoElements.length; k++) {
+                logoElements[k].classList.add("hidden");
+            }
+
 
             document.getElementsByClassName(dataContent)[0].classList.remove("hidden");
             document.getElementById(dataContent).classList.remove("hidden");
+            document.getElementsByClassName(dataLogo)[0].classList.remove("hidden");
+            document.getElementById(dataLogo).classList.remove("hidden");
         });
     }
+
+
+    const logo = document.getElementsByClassName
 });
